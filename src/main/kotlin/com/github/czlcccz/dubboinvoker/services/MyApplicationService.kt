16cdcc6cbafb2a9bdc4b2f15901d839309187bee
GitHub Action <action@github.com>
2023-01-06 07:04:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/czlcccz/dubboinvoker/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/czlcccz/dubboinvoker/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.czlcccz.dubboinvoker.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.czlcccz.dubboinvoker.MyBundle
 
 class MyApplicationService {
 
